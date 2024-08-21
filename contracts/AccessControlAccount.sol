abce59a46a39566cc1c2b32dbe4b2fd1b5d6a1d8
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2024-08-21 08:18:26 -0300

    First commit (pretty complete!)
    Boilerplate + initial contract AccessControlAccount.

---
+++ contracts/AccessControlAccount.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.23;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {BaseAccount} from "@account-abstraction/contracts/core/BaseAccount.sol";
+import {SIG_VALIDATION_SUCCESS, SIG_VALIDATION_FAILED} from "@account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {PackedUserOperation} from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract AccessControlAccount is AccessControl, BaseAccount {
+  bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
+  bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
+
+  IEntryPoint private immutable _entryPoint;
+
+  error RequiredEntryPointOrExecutor(address sender);
+  error WrongArrayLength();
+
+  /// @inheritdoc BaseAccount
+  function entryPoint() public view virtual override returns (IEntryPoint) {
+    return _entryPoint;
+  }
+
+  // solhint-disable-next-line no-empty-blocks
+  receive() external payable {}
+
+  constructor(IEntryPoint anEntryPoint, address admin, address[] memory executors) {
+    _entryPoint = anEntryPoint;
+    _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    for (uint256 i; i < executors.length; i++) {
+      _grantRole(EXECUTOR_ROLE, executors[i]);
+    }
+  }
+
+  // Require the function call went through EntryPoint or owner
+  function _requireFromEntryPointOrExecutor() internal view {
+    if (msg.sender != address(entryPoint()) && !hasRole(EXECUTOR_ROLE, msg.sender))
+      revert RequiredEntryPointOrExecutor(msg.sender);
+  }
+
+  /**
+   * execute a transaction (called directly from owner, or by entryPoint)
+   * @param dest destination address to call
+   * @param value the value to pass in this call
+   * @param func the calldata to pass in this call
+   */
+  function execute(address dest, uint256 value, bytes calldata func) external {
+    _requireFromEntryPointOrExecutor();
+    _call(dest, value, func);
+  }
+
+  /**
+   * execute a sequence of transactions
+   * @dev to reduce gas consumption for trivial case (no value), use a zero-length array to mean zero value
+   * @param dest an array of destination addresses
+   * @param value an array of values to pass to each call. can be zero-length for no-value calls
+   * @param func an array of calldata to pass to each call
+   */
+  function executeBatch(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) external {
+    _requireFromEntryPointOrExecutor();
+    if (dest.length != func.length || (value.length != 0 && value.length != func.length)) revert WrongArrayLength();
+    if (value.length == 0) {
+      for (uint256 i = 0; i < dest.length; i++) {
+        _call(dest[i], 0, func[i]);
+      }
+    } else {
+      for (uint256 i = 0; i < dest.length; i++) {
+        _call(dest[i], value[i], func[i]);
+      }
+    }
+  }
+
+  /// implement template method of BaseAccount
+  function _validateSignature(
+    PackedUserOperation calldata userOp,
+    bytes32 userOpHash
+  ) internal virtual override returns (uint256 validationData) {
+    bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+    address recovered = ECDSA.recover(hash, userOp.signature);
+    if (!hasRole(EXECUTOR_ROLE, recovered)) return SIG_VALIDATION_FAILED;
+    return SIG_VALIDATION_SUCCESS;
+  }
+
+  function _call(address target, uint256 value, bytes memory data) internal {
+    (bool success, bytes memory result) = target.call{value: value}(data);
+    if (!success) {
+      // solhint-disable-next-line no-inline-assembly
+      assembly {
+        revert(add(result, 32), mload(result))
+      }
+    }
+  }
+
+  /**
+   * check current account deposit in the entryPoint
+   */
+  function getDeposit() public view returns (uint256) {
+    return entryPoint().balanceOf(address(this));
+  }
+
+  /**
+   * deposit more funds for this account in the entryPoint
+   */
+  function addDeposit() public payable {
+    entryPoint().depositTo{value: msg.value}(address(this));
+  }
+
+  /**
+   * withdraw value from the account's deposit
+   * @param withdrawAddress target to send to
+   * @param amount to withdraw
+   */
+  function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public onlyRole(WITHDRAW_ROLE) {
+    entryPoint().withdrawTo(withdrawAddress, amount);
+  }
+}
---
+++ test/test-access-control-account.js
@@ -0,0 +1,145 @@
+const { expect } = require("chai");
+const { _W, getRole, amountFunction, getAddress } = require("@ensuro/core/js/utils");
+const { setupChain, initForkCurrency } = require("@ensuro/core/js/test-utils");
+const hre = require("hardhat");
+
+const { ethers } = hre;
+const { MaxUint256 } = hre.ethers;
+
+const _A = amountFunction(6);
+const ADDRESSES = {
+  // polygon mainnet addresses
+  USDC: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+  USDCWhale: "0x4d97dcd97ec945f40cf65f87097ace5ea0476045",
+  ENTRYPOINT: "0x0000000071727De22E5E9d8BAf0edAc6f37da032",
+};
+
+async function setUp() {
+  await setupChain(null);
+  const [, exec1, exec2, anon, withdraw, admin] = await ethers.getSigners();
+
+  const usdc = await initForkCurrency(ADDRESSES.USDC, ADDRESSES.USDCWhale, [exec1, exec2], [_A(100), _A(100)]);
+  const ep = await ethers.getContractAt("IEntryPoint", ADDRESSES.ENTRYPOINT);
+  const AccessControlAccount = await ethers.getContractFactory("AccessControlAccount");
+  const acAcc = await AccessControlAccount.deploy(ep, admin, [exec1, exec2]);
+  const roles = {
+    admin: getRole("DEFAULT_ADMIN_ROLE"),
+    exec: getRole("EXECUTOR_ROLE"),
+    withdraw: getRole("WITHDRAW_ROLE"),
+  };
+
+  return {
+    ep,
+    AccessControlAccount,
+    acAcc,
+    exec1,
+    exec2,
+    anon,
+    withdraw,
+    admin,
+    roles,
+    usdc,
+  };
+}
+
+describe("AccessControlAccount contract tests", function () {
+  it("Constructs with the right permissions and EP", async () => {
+    const { acAcc, anon, exec1, exec2, admin, roles } = await setUp();
+    expect(await acAcc.hasRole(roles.admin, admin)).to.equal(true);
+    expect(await acAcc.hasRole(roles.admin, anon)).to.equal(false);
+    expect(await acAcc.hasRole(roles.exec, exec1)).to.equal(true);
+    expect(await acAcc.hasRole(roles.exec, exec2)).to.equal(true);
+    expect(await acAcc.hasRole(roles.exec, anon)).to.equal(false);
+    expect(await acAcc.entryPoint()).to.equal(ADDRESSES.ENTRYPOINT);
+  });
+
+  it("Can receive eth, deposits and only WITHDRAW_ROLE can withdraw", async () => {
+    const { acAcc, anon, withdraw, admin, roles, ep } = await setUp();
+    expect(await hre.ethers.provider.getBalance(acAcc)).to.equal(0);
+    await expect(() => withdraw.sendTransaction({ to: acAcc, value: _W(1) })).to.changeEtherBalance(acAcc, _W(1));
+    expect(await hre.ethers.provider.getBalance(acAcc)).to.equal(_W(1));
+
+    await expect(() => acAcc.addDeposit({ value: _W(2) })).to.changeEtherBalance(ep, _W(2));
+    expect(await ep.balanceOf(acAcc)).to.equal(_W(2));
+
+    await expect(acAcc.connect(withdraw).withdrawDepositTo(anon, _W(1)))
+      .to.be.revertedWithCustomError(acAcc, "AccessControlUnauthorizedAccount")
+      .withArgs(withdraw, roles.withdraw);
+
+    await expect(acAcc.connect(anon).grantRole(roles.withdraw, withdraw))
+      .to.be.revertedWithCustomError(acAcc, "AccessControlUnauthorizedAccount")
+      .withArgs(anon, roles.admin);
+
+    await expect(acAcc.connect(admin).grantRole(roles.withdraw, withdraw)).not.to.be.reverted;
+
+    await expect(() => acAcc.connect(withdraw).withdrawDepositTo(anon, _W("0.5"))).to.changeEtherBalance(
+      anon,
+      _W("0.5")
+    );
+    expect(await ep.balanceOf(acAcc)).to.equal(_W("1.5"));
+    expect(await acAcc.getDeposit()).to.equal(_W("1.5"));
+  });
+
+  it("Can execute when called directly", async () => {
+    const { acAcc, anon, exec1, usdc } = await setUp();
+    const approveExec1 = usdc.interface.encodeFunctionData("approve", [getAddress(exec1), MaxUint256]);
+    await expect(acAcc.connect(anon).execute(usdc, 0, approveExec1))
+      .to.be.revertedWithCustomError(acAcc, "RequiredEntryPointOrExecutor")
+      .withArgs(anon);
+    expect(await usdc.allowance(acAcc, exec1)).to.equal(0);
+    await expect(acAcc.connect(exec1).execute(usdc, 0, approveExec1)).not.to.be.reverted;
+    expect(await usdc.allowance(acAcc, exec1)).to.equal(MaxUint256);
+  });
+
+  it("Can executeBatch when called directly", async () => {
+    const { acAcc, anon, exec1, exec2, usdc } = await setUp();
+
+    // Setup - send some initial money
+    await usdc.connect(exec1).transfer(acAcc, _A(10));
+    await usdc.connect(exec2).transfer(acAcc, _A(20));
+    expect(await usdc.balanceOf(acAcc)).to.equal(_A(30));
+
+    const calls = [
+      usdc.interface.encodeFunctionData("transfer", [getAddress(exec1), _A(5)]),
+      usdc.interface.encodeFunctionData("transfer", [getAddress(exec2), _A(10)]),
+    ];
+    await expect(acAcc.connect(anon).executeBatch([usdc, usdc], [], calls))
+      .to.be.revertedWithCustomError(acAcc, "RequiredEntryPointOrExecutor")
+      .withArgs(anon);
+    await expect(() => acAcc.connect(exec2).executeBatch([usdc, usdc], [], calls)).to.changeTokenBalances(
+      usdc,
+      [exec1, exec2],
+      [_A(5), _A(10)]
+    );
+    expect(await usdc.balanceOf(acAcc)).to.equal(_A(15));
+  });
+
+  it.only("Can executeBatch when called directly (with value)", async () => {
+    const { acAcc, anon, exec1, exec2, ep } = await setUp();
+
+    // Setup - send some eth to acAcc and deposit
+    await expect(() => acAcc.addDeposit({ value: _W(5) })).to.changeEtherBalance(ep, _W(5));
+    expect(await ep.balanceOf(acAcc)).to.equal(_W(5));
+    await expect(() => exec1.sendTransaction({ to: acAcc, value: _W(3) })).to.changeEtherBalance(acAcc, _W(3));
+
+    const calls = [
+      ep.interface.encodeFunctionData("depositTo", [getAddress(exec2)]),
+      ep.interface.encodeFunctionData("addStake", [3600]),
+    ];
+    await expect(acAcc.connect(exec1).executeBatch([ep, ep], [_W(1)], calls)).to.be.revertedWithCustomError(
+      acAcc,
+      "WrongArrayLength"
+    );
+    await expect(acAcc.connect(exec1).executeBatch([ep], [], calls)).to.be.revertedWithCustomError(
+      acAcc,
+      "WrongArrayLength"
+    );
+    await expect(() => acAcc.connect(exec1).executeBatch([ep, ep], [_W(1), _W(2)], calls)).to.changeEtherBalance(
+      ep,
+      _W(3)
+    );
+    expect(await ep.balanceOf(acAcc)).to.equal(_W(5));
+    expect(await ep.balanceOf(exec2)).to.equal(_W(1)); // The deposit was made on behalft of exec2
+    expect((await ep.getDepositInfo(acAcc)).stake).to.equal(_W(2));
+  });
+});
